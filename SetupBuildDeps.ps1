f88bb71e6dbcff5f7c02beecbd803e7ba3cecfa0
Wes Peter <wes@wespeter.com>
2017-10-03 09:43:56 -0700

    set environment variables

--- SetupBuildDeps.ps1
+++ SetupBuildDeps.ps1
@@ -1,3 +1,6 @@
+"creating powershell profile"
+New-item –type file –force $profile
+
 "Installing chocolatey package manager"
 Set-ExecutionPolicy Bypass; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
@@ -6,3 +9,12 @@ choco install dotnetcore-sdk -y
 
 "Installing node and npm"
 choco install nodejs.install -y
+
+"adding NPM to system variables"
+[Environment]::SetEnvironmentVariable("npm", "C:\Program Files\nodejs\npm.cmd", "Machine")
+
+"adding dotnet to system variables"
+[Environment]::SetEnvironmentVariable("dotnet", "C:\Program Files\dotnet\dotnet.exe", "Machine")
+
+# finally refresh environment using chocolatey's script
+refreshenv
